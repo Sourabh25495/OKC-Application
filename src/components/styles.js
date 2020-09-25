aff08f3d3592332855a424fc96614ce1602b7616
Sourabh Kulkarni <sourabhkulkarni@sourabhs-air.lan>
2020-09-25 16:51:37 -0400

    Button txt color

--- src/components/styles.js
+++ src/components/styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles((theme) => ({
   muiButton: {
     backgroundColor: "#00CC00",
+    color: "#FFFFFF",
   },
   textField: {
     backgroundColor: "#FFFFFF"
