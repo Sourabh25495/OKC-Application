20afa5095cef01f8ed6952b28141ace1a894fc74
Sourabh Kulkarni <sourabhkulkarni@sourabhs-air.lan>
2020-09-25 13:05:20 -0400

    OKC essay functionality

--- src/components/InputComponent/InputComponent.js
+++ src/components/InputComponent/InputComponent.js
@@ -2,13 +2,13 @@ import React from "react";
 import { TextField } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { SUBMIT_FIELD } from "../../madlibs";
-import {useStyles} from '../styles'
+import { useStyles } from "../styles";
 
 export const InputComponent = ({ label, id, staticText }) => {
   const dispatch = useDispatch();
-  const classes = useStyles()
+  const classes = useStyles();
   return (
-    <div style={{ display: "grid", paddingTop: "30px" }}>
+    <div className="input-component">
       <div className="field-label">
         <label>{label}</label>
       </div>
@@ -17,11 +17,12 @@ export const InputComponent = ({ label, id, staticText }) => {
           variant="outlined"
           className={classes.textField}
           onBlur={(e) =>
-            e.target.value && dispatch({
+            e.target.value &&
+            dispatch({
               type: SUBMIT_FIELD,
               payload: {
                 fieldName: id,
-                answer: `${e.target.value}${e.target.value ? '.' : ''}`,
+                answer: `${e.target.value}${e.target.value ? "." : ""}`,
                 staticStatement: staticText,
               },
             })
